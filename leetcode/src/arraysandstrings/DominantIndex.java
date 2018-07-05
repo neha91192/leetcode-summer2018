ce5f0db0041760f88cd81729af7fdc8f9c511c35
Neha <shukla.ne@husky.neu.edu>
2018-07-05 14:34:11 -0400

    updated problem description

--- leetcode/src/arraysandstrings/DominantIndex.java
+++ leetcode/src/arraysandstrings/DominantIndex.java
@@ -1,10 +1,13 @@
 package arraysandstrings;
 
 /**
- * In a given integer array nums, there is always exactly one largest element.
- * Find whether the largest element in the array is at least twice as much as
- * every other number in the array. If it is, return the index of the largest
- * element, otherwise return -1.
+ * <Problem 747> In a given integer array nums, there is always exactly one
+ * largest element. Find whether the largest element in the array is at least
+ * twice as much as every other number in the array. If it is, return the index
+ * of the largest element, otherwise return -1.
+ * 
+ * Link ->
+ * https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
  */
 
 public class DominantIndex {
--- leetcode/src/arraysandstrings/FindPivotIndex.java
+++ leetcode/src/arraysandstrings/FindPivotIndex.java
@@ -1,5 +1,18 @@
 package arraysandstrings;
 
+/**
+ * <Problem 724> Given an array of integers nums, write a method that returns
+ * the "pivot" index of this array. We define the pivot index as the index where
+ * the sum of the numbers to the left of the index is equal to the sum of the
+ * numbers to the right of the index.
+ * 
+ * If no such index exists, we should return -1. If there are multiple pivot
+ * indexes, you should return the left-most pivot index.
+ * 
+ * Link -> https://leetcode.com/problems/find-pivot-index/description/
+ * 
+ */
+
 public class FindPivotIndex {
 	/**
 	 * Calculate total sum. Then for each candidate pivotal index in nums, calculate
